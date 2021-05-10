a390d339d00cca28a9ea89fb429edd8f733c5506
Harshwardhan Mehrotra <harshwardhanmehrotra@gmail.com>
2021-05-11 01:09:03 +0530

    Created new repo for api microservic

---
+++ src/main/java/com/codingcalendar/coding_calendar_api/CodingCalendarApiApplication.java
@@ -0,0 +1,13 @@
+package com.codingcalendar.coding_calendar_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingCalendarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingCalendarApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingcalendar/coding_calendar_api/CodingCalendarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingcalendar.coding_calendar_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingCalendarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
