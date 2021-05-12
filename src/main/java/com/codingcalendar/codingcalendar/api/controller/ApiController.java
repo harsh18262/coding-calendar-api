c9f565b6a33b82f66803ba0d2325c831d84ec9ab
Harshwardhan Mehrotra <harshwardhanmehrotra@gmail.com>
2021-05-12 12:13:11 +0530

    Allowed all orgins for CORS

--- src/main/java/com/codingcalendar/codingcalendar/api/controller/ApiController.java
+++ src/main/java/com/codingcalendar/codingcalendar/api/controller/ApiController.java
@@ -1,18 +1,25 @@
 package com.codingcalendar.codingcalendar.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codingcalendar.codingcalendar.api.Entities.Contest;
+import com.codingcalendar.codingcalendar.api.Entities.ContestRepository;
 import com.codingcalendar.codingcalendar.api.Services.ApiServices;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ApiController {
 		
 		
 		@Autowired
 		ApiServices apiservices;
+		@Autowired
+		ContestRepository contestrepo;
 	
 		@GetMapping("/test")
 		public String test()
@@ -21,8 +28,15 @@ public class ApiController {
 		}
 
 		@GetMapping("/all")
-		public Iterable<Contest> getall()
+		public ResponseEntity<Iterable<Contest>> getall()
 		{
-			return  apiservices.getAllcontests();
+			return  ResponseEntity.ok(apiservices.getAllcontests());
 		}
+		
+		@GetMapping("/error1")
+		public ResponseEntity error()
+		{
+			return  ResponseEntity.ok(contestrepo.findAll());
+		}
+
 }
